b04ba7a8da3ebcc2f3fe445169d7674045c9708c
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-05-28 15:27:30 -0300

    produtos e associação muitos para muitos

---
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.luishenrique.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luishenrique.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
