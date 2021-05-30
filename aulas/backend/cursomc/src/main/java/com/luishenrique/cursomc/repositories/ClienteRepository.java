ef7ad5809e3efe3638f466c0b5b20f4f550e581f
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-05-30 01:14:25 -0300

    Clientes, tipoClientes, Telefones e endereços

---
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,11 @@
+package com.luishenrique.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luishenrique.cursomc.domain.Cliente;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
