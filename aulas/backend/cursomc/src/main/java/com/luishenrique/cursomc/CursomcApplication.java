0094eccb38a5e0bbc2d56805410b01ed768ab5c7
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-05-31 22:34:16 -0300

    ItemPedido e ItemPedidoPK

--- aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/CursomcApplication.java
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/CursomcApplication.java
@@ -13,6 +13,7 @@ import com.luishenrique.cursomc.domain.Cidade;
 import com.luishenrique.cursomc.domain.Cliente;
 import com.luishenrique.cursomc.domain.Endereco;
 import com.luishenrique.cursomc.domain.Estado;
+import com.luishenrique.cursomc.domain.ItemPedido;
 import com.luishenrique.cursomc.domain.Pagamento;
 import com.luishenrique.cursomc.domain.PagamentoComBoleto;
 import com.luishenrique.cursomc.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.luishenrique.cursomc.repositories.CidadeRepository;
 import com.luishenrique.cursomc.repositories.ClienteRepository;
 import com.luishenrique.cursomc.repositories.EnderecoRepository;
 import com.luishenrique.cursomc.repositories.EstadoRepository;
+import com.luishenrique.cursomc.repositories.ItemPedidoRepository;
 import com.luishenrique.cursomc.repositories.PagamentoRepository;
 import com.luishenrique.cursomc.repositories.PedidoRepository;
 import com.luishenrique.cursomc.repositories.ProdutoRepository;
@@ -50,6 +52,8 @@ public class CursomcApplication implements CommandLineRunner
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args)
 	{
@@ -117,6 +121,19 @@ public class CursomcApplication implements CommandLineRunner
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.0, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 
 	}
 //Banco temporario: http://localhost:8080/h2-console
--- aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/domain/Produto.java
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/domain/Produto.java
@@ -2,7 +2,9 @@ package com.luishenrique.cursomc.domain;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,11 +13,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
-public class Produto implements Serializable {
+public class Produto implements Serializable
+{
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -28,51 +32,83 @@ public class Produto implements Serializable {
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
 
-	public Produto() {
+	@OneToMany(mappedBy = "id.produto")
+	private Set<ItemPedido> itens = new HashSet<>();
+
+	public Produto()
+	{
 		super();
 	}
 
-	public Produto(Integer id, String nome, Double preco) {
+	public Produto(Integer id, String nome, Double preco)
+	{
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.preco = preco;
 	}
 
-	public Integer getId() {
+	public List<Pedido> getPedidos()
+	{
+		List<Pedido> list = new ArrayList<>();
+		for (ItemPedido x : itens)
+			list.add(x.getPedido());
+		return list;
+	}
+
+	public Integer getId()
+	{
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Integer id)
+	{
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getNome()
+	{
 		return nome;
 	}
 
-	public void setNome(String nome) {
+	public void setNome(String nome)
+	{
 		this.nome = nome;
 	}
 
-	public Double getPreco() {
+	public Double getPreco()
+	{
 		return preco;
 	}
 
-	public void setPreco(Double preco) {
+	public void setPreco(Double preco)
+	{
 		this.preco = preco;
 	}
 
-	public List<Categoria> getCategorias() {
+	public List<Categoria> getCategorias()
+	{
 		return categorias;
 	}
 
-	public void setCategorias(List<Categoria> categorias) {
+	public void setCategorias(List<Categoria> categorias)
+	{
 		this.categorias = categorias;
 	}
 
+	public Set<ItemPedido> getItens()
+	{
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens)
+	{
+		this.itens = itens;
+	}
+
 	@Override
-	public int hashCode() {
+	public int hashCode()
+	{
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
@@ -80,7 +116,8 @@ public class Produto implements Serializable {
 	}
 
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj)
+	{
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -88,7 +125,8 @@ public class Produto implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Produto other = (Produto) obj;
-		if (id == null) {
+		if (id == null)
+		{
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
