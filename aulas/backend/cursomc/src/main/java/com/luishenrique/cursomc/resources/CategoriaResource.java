884b01485fb78160f24f29215db953409fd8f6b6
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-06-04 15:25:26 -0300

    Validação sintática com Beans validation

--- aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/resources/CategoriaResource.java
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,16 +37,18 @@ public class CategoriaResource
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria obj)
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO objDto)
 	{
+		Categoria obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id)
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id)
 	{
+		Categoria obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
@@ -56,28 +60,27 @@ public class CategoriaResource
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll()
 	{
 		List<Categoria> list = service.findAll();
-		//Converte uma lista de Categoria para uma lista CategoriaDTO
+		// Converte uma lista de Categoria para uma lista CategoriaDTO
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
-	
+
 	@RequestMapping(value = "/page", method = RequestMethod.GET)
-	public ResponseEntity<Page<CategoriaDTO>> findPage(
-			@RequestParam(value = "page", defaultValue = "0") Integer page, 
-			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage, 
-			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy, 
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction)
 	{
 		Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
-		
-		//Converte uma lista de Categoria para uma lista CategoriaDTO
+
+		// Converte uma lista de Categoria para uma lista CategoriaDTO
 		Page<CategoriaDTO> listDto = list.map(obj -> new CategoriaDTO(obj));
-		
+
 		return ResponseEntity.ok().body(listDto);
 	}
 
--- aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/services/CategoriaService.java
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.luishenrique.cursomc.domain.Categoria;
+import com.luishenrique.cursomc.dto.CategoriaDTO;
 import com.luishenrique.cursomc.repositories.CategoriaRepository;
 import com.luishenrique.cursomc.services.exceptions.DataIntegrityException;
 import com.luishenrique.cursomc.services.exceptions.ObjectNotFoundException;
@@ -64,5 +65,10 @@ public class CategoriaService
 	{
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(CategoriaDTO objDto)
+	{
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
