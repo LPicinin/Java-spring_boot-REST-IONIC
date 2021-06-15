87d4e4b4b3e76626873553512d357e2217af4d8e
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-06-15 15:43:16 -0300

    testando e ajustando a deleção de cliente

--- aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/domain/Cliente.java
+++ aulas/backend/cursomc/src/main/java/com/luishenrique/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -29,7 +30,9 @@ public class Cliente implements Serializable
 	private String cpfOuCnpj;
 	private Integer tipo;
 
-	@OneToMany(mappedBy = "cliente")
+	// cascade = toda operação que modificar um cliente reflete em cascata nos
+	// endereços
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	// substitui a implementação de classes simples, que só teriam um atributo
@@ -53,7 +56,7 @@ public class Cliente implements Serializable
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj = cpfOuCnpj;
-		this.tipo = tipo == null? null : tipo.getCod();
+		this.tipo = tipo == null ? null : tipo.getCod();
 	}
 
 	public Integer getId()
