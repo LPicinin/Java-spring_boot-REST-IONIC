19bd1a15e34a4fb5fc18566ef42e36eefae4b9c2
Luís Henrique Picinin Jandre <35941867+LPicinin@users.noreply.github.com>
2021-07-14 15:46:27 -0300

    email-html adicionado

--- src/main/java/com/luishenrique/cursomc/services/AbstractEmailService.java
+++ src/main/java/com/luishenrique/cursomc/services/AbstractEmailService.java
@@ -2,8 +2,16 @@ package com.luishenrique.cursomc.services;
 
 import java.util.Date;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.luishenrique.cursomc.domain.Pedido;
 
@@ -11,6 +19,12 @@ public abstract class AbstractEmailService implements EmailService
 {
 	@Value("${default.sender}")
 	private String sender;
+
+	@Autowired
+	private TemplateEngine templateEngine;
+
+	@Autowired
+	private JavaMailSender javaMailSender;
 
 	@Override
 	public void sendOrderConfirmationEmail(Pedido obj)
@@ -30,4 +44,37 @@ public abstract class AbstractEmailService implements EmailService
 		return sm;
 	}
 
+	protected String htmlFromTemplatePedido(Pedido obj)
+	{
+		Context context = new Context();
+		context.setVariable("pedido", obj);
+		return templateEngine.process("email/confirmacaoPedido", context);
+	}
+
+	@Override
+	public void sendOrderConfirmationHtmlEmail(Pedido obj)
+	{
+		try
+		{
+			MimeMessage mimeMessager = prepareMimeMessageFromPedido(obj);
+			sendHtmlEmail(mimeMessager);
+		} catch (MessagingException e)
+		{
+			sendOrderConfirmationEmail(obj);
+		}
+
+	}
+
+	protected MimeMessage prepareMimeMessageFromPedido(Pedido obj) throws MessagingException
+	{
+		MimeMessage mimeMessager = javaMailSender.createMimeMessage();
+		MimeMessageHelper mimeMessagerHelper = new MimeMessageHelper(mimeMessager, true);
+		mimeMessagerHelper.setTo(obj.getCliente().getEmail());
+		mimeMessagerHelper.setFrom(sender);
+		mimeMessagerHelper.setSubject("Pedido Confirmado! Código: " + obj.getId());
+		mimeMessagerHelper.setSentDate(new Date(System.currentTimeMillis()));
+		mimeMessagerHelper.setText(htmlFromTemplatePedido(obj), true);
+
+		return mimeMessager;
+	}
 }
--- src/main/java/com/luishenrique/cursomc/services/SmtpEmailService.java
+++ src/main/java/com/luishenrique/cursomc/services/SmtpEmailService.java
@@ -1,25 +1,38 @@
 package com.luishenrique.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 public class SmtpEmailService extends AbstractEmailService
 {
 	private static final Logger LOG = LoggerFactory.getLogger(SmtpEmailService.class);
-	
+
 	@Autowired
 	private MailSender mailSender;
-	
-	
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
 	@Override
 	public void sendEmail(SimpleMailMessage message)
 	{
 		LOG.info("Enviando email...");
 		mailSender.send(message);
 		LOG.info("Email enviado!");
+	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage message)
+	{
+		LOG.info("Enviando email HTML...");
+		javaMailSender.send(message);
+		LOG.info("Email HTML enviado!");
 	}
 
 }
